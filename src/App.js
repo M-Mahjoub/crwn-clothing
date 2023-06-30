f01566d18f783b42b60ebd5fb323685905ab58e5
M-Mahjoub <M-Mahjoub@gmail.com>
2023-06-30 21:49:25 +0330

    updating with collection components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import "./App.css";
-import  HomePage  from "./homepag.component";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+
+import { Link, Route, Routes } from "react-router-dom";
+
+const TopicsList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <button onClick={() => props.history.push("/topics")}>Topics</button>
+      <Link to="/topics">Topics</Link>
+      <h1>Topic Detail Page</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <HomePage></HomePage>
+  return (
+    <Routes>
+      <Route path="/" Component={HomePage} />
+      <Route path="/shop" Component={ShopPage} />
+      <Route path="/topics/:topicId" Component={TopicDetail} />
+    </Routes>
+  );
 }
 
 export default App;
