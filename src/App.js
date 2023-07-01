b16ef6ea13b79d93db15fb3f923686d70c245175
M-Mahjoub <M-Mahjoub@gmail.com>
2023-07-01 20:45:39 +0330

    .

--- src/App.js
+++ src/App.js
@@ -2,35 +2,18 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
-import { Link, Route, Routes } from "react-router-dom";
-
-const TopicsList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Topic List Page</h1>
-    </div>
-  );
-};
-
-const TopicDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push("/topics")}>Topics</button>
-      <Link to="/topics">Topics</Link>
-      <h1>Topic Detail Page</h1>
-    </div>
-  );
-};
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" Component={HomePage} />
-      <Route path="/shop" Component={ShopPage} />
-      <Route path="/topics/:topicId" Component={TopicDetail} />
-    </Routes>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/shop" Component={ShopPage} />
+      </Routes>
+    </div>
   );
 }
 
